bb5ec8806d8c5c96efdfa20e138de96526a26c3c
kizuner-bonely <1003306162@qq.com>
2022-07-24 16:08:10 +0800

    chore: 优化环形链表解法

--- src/linkedList/practice/leetCode-141-环形链表.ts
+++ src/linkedList/practice/leetCode-141-环形链表.ts
@@ -3,11 +3,10 @@ import type { ListNode } from './index.type'
 export function hasCycle(head: ListNode | null): boolean {
   if (!head || !head.next) return false
   let p: ListNode | null = head,
-    q: ListNode | null = head.next
-  while (q?.next && p !== q) {
+    q: ListNode | null = head
+  do {
     p = p!.next
-    q = q.next.next
-  }
-  if (p === q) return true
-  return false
+    q = q.next?.next ?? null
+  } while (q?.next && p !== q)
+  return p === q
 }
